a6a44eb0a30523c7ee32276a1425864424b73294
TaljaaEb <138305368+TaljaaEb@users.noreply.github.com>
2025-08-24 07:55:29 +0100

    Create include.js

---
+++ Msg/include.js
@@ -0,0 +1,31 @@
+<script>
+  fetch("http://127.0.0.1:8889/ipc", { 
+    method: "POST",
+    headers: {
+      "Content-Type": "text/plain"
+    }
+  })
+  .then(res => res.text())
+  .then(data => {
+    console.log("Received from service:", data);
+    // you can assign it to a JS variable
+    var ipcValue = data;
+  })
+  .catch(err => console.error("IPC send error:", err));
+</script>
+<script>
+  // --- Send new message to service ---
+  function sendToService(msg) {
+    fetch("http://127.0.0.1:8889/ipc", { 
+      method: "POST",
+      headers: { "Content-Type": "text/plain" },
+      body: msg
+    })
+    .then(res => res.text())
+    .then(data => console.log("Service response:", data))
+    .catch(err => console.error("IPC send error:", err));
+  }
+
+  // Example usage:
+  sendToService("12345");
+</script>
